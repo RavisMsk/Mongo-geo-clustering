68c71594a7578f3d102de900628dcc574707414a
Nikita Anisimov <ravis.bmstu@gmail.com>
2015-05-26 22:09:53 +0300

    Un-needed reqs

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var geo = require('./routes/geo');
